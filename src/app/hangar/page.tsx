47779337028f743a55bc4992e459d303fcc6d439
PaoAguilar <pao.aquev29@gmail.com>
2025-08-18 19:54:44 -0600

    ✨ commented labels

--- src/app/hangar/page.tsx
+++ src/app/hangar/page.tsx
@@ -11,8 +11,8 @@ export default function HangarPage() {
     <div>
       {!showHangar && (
         <div className="min-h-[calc(100dvh-104px-91.83px)] text-white flex flex-col justify-center items-center">
-          <h2 className="text-[34px]">one mission, one match, one destiny</h2>
-          <h2 className="text-[34px]">Make your choise!</h2>
+          {/* <h2 className="text-[34px]">one mission, one match, one destiny</h2>
+          <h2 className="text-[34px]">Make your choise!</h2> */}
           <Button
             variant="secondary"
             className="rounded-full min-w-28"

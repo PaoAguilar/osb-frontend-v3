91b3ccd309bced50dda694d9c0ab52078779af30
PaoAguilar <pao.aquev29@gmail.com>
2025-10-27 18:36:26 -0600

    🐛 fix navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,31 +1,15 @@
 "use client";
-import { useAuth } from "@/contexts/AuthContext";
 import SiteLogo from "../siteLogo/SiteLogo";
 import NavMenu from "./NavMenu";
 import UserProfile from "./UserProfile";
 import { navLinks } from "./navLinks";
 
 const Navbar = () => {
-  const { isAuthenticated, isGuest, user } = useAuth();
-
-  console.log("user", user)
-  const userHref = isAuthenticated ? "/profile" : "/login";
-  const userName = isAuthenticated
-    ? isGuest
-      ? "Guest"
-      : user?.username || "User"
-    : "Login";
-
-
   return (
     <header className="flex h-[6.5rem] items-center justify-between p-4 bg-transparent">
       <SiteLogo size="medium" imageSrc="/img/logo_horizontal.svg" />
       <NavMenu links={navLinks} />
-      <UserProfile
-        name={userName}
-        avatar={"/img/user-example.jpg"}
-        href={userHref}
-      />
+      <UserProfile className="" href="/profile" />
     </header>
   );
 };
